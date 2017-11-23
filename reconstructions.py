a5c06b8992b1ad2ed651277c1f54229847b7cc44
Matteo Abis <gmatteo..abis@gmail.com>
2017-11-23 14:17:48 +0100

    add centers

--- reconstructions.py
+++ reconstructions.py
@@ -1,7 +1,5 @@
-from tomopy.recon.rotation import find_center_pc
-import os.path
-from glob import glob
-import tifffile
+import numpy as np
+import subprocess
 
 folders = [
     "KO202_LL_control_1",
@@ -31,15 +29,8 @@ folders = [
     "WT355_LL_smoke_3"
 ]
 
-for folder in folders:
-    base_folder = "/sls/X02DA/data/e13657/Data10/matteo_high_resolution/"
-    input_filenames = sorted(glob(os.path.join(base_folder, folder, "tif", "*.tif")))
-    first_name = input_filenames[130]
-    last_name = input_filenames[130 + 1800]
-    first = tifffile.imread(first_name)
-    last = tifffile.imread(last_name)
-    center = find_center_pc(
-        first, last,
-        tol=0.1,
-    )
-    print(center)
+centers = np.loadtxt("centers.log")
+for center, folder in zip(centers, folders):
+    command="prj2sinSGE -d -k 0 -I 0 -R 0 -g 0 -f 1801,0,0,0,0 -p {0}####.fltp.DMP -Z 0.5 -c {1} -F schepp -t 16 --jobname={0}_reco --queue=tomcat_offline.q -o /sls/X02DA/Data10/e13657/matteo_high_resolution/{0}/sino_tmp/ -O /sls/X02DA/Data10/e13657/matteo_high_resolution/{0}/rec_16bit/ /sls/X02DA/Data10/e13657/matteo_high_resolution/{0}/fltp/;".format(folder, center)
+    print(command)
+    subprocess.call(command.format(folder), shell=True)
